cde4eac7491b337ca4ce7d8c3eb2e7de04112273
William Ravelo <drummerwilliam@gmail.com>
2018-05-04 07:36:55 -0500

    added bar chart

--- imports/data.js
+++ imports/data.js
@@ -1,26 +1,27 @@
 export function getUpc() {
-	return [{
-		vote_count: 389,
-		id: 427641,
-		video: false,
-		vote_average: 5.9,
-		title: "Rampage",
-		popularity: 188.516677,
-		poster_path: "/30oXQKwibh0uANGMs0Sytw3uN22.jpg",
-		original_language: "en",
-		original_title: "Rampage",
-		genre_ids: [
-			28,
-			12,
-			878
+	return [
+		{
+			vote_count: 389,
+			id: 427641,
+			video: false,
+			vote_average: 5.9,
+			title: "Rampage",
+			popularity: 188.516677,
+			poster_path: "/30oXQKwibh0uANGMs0Sytw3uN22.jpg",
+			original_language: "en",
+			original_title: "Rampage",
+			genre_ids: [
+				28,
+				12,
+				878
 
-		],
-		backdrop_path: "/wrqUiMXttHE4UBFMhLHlN601MZh.jpg",
-		adult: false,
-		overview: "Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth. But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size. To make matters worse, it’s soon discovered there are other similarly altered animals. As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.",
-		release_date: "2018-04-12"
+			],
+			backdrop_path: "/wrqUiMXttHE4UBFMhLHlN601MZh.jpg",
+			adult: false,
+			overview: "Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth. But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size. To make matters worse, it’s soon discovered there are other similarly altered animals. As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.",
+			release_date: "2018-04-12"
 
-	},
+		},
 		{
 			vote_count: 847,
 			id: 447332,
@@ -43,9 +44,715 @@ export function getUpc() {
 			overview: "A family is forced to live in silence while hiding from creatures that hunt by sound.",
 			release_date: "2018-04-05"
 
-		}];
+		},
+		{
+			vote_count: 166,
+			id: 381719,
+			video: false,
+			vote_average: 6.5,
+			title: "Peter Rabbit",
+			popularity: 43.243614,
+			poster_path: "/2yjSvEDuM3rLDng40erLsWkQRfn.jpg",
+			original_language: "en",
+			original_title: "Peter Rabbit",
+			genre_ids: [
+				16,
+				12,
+				10751
+
+			],
+			backdrop_path: "/e9dVo1OXZL6sWinc9uIS0Foo4QG.jpg",
+			adult: false,
+			overview: "Peter Rabbit's feud with Mr. McGregor escalates to greater heights than ever before as they rival for the affections of the warm-hearted animal lover who lives next door.",
+			release_date: "2018-02-07"
+
+		},
+		{
+			vote_count: 471,
+			id: 429351,
+			video: false,
+			vote_average: 4.9,
+			title: "12 Strong",
+			popularity: 37.716405,
+			poster_path: "/j18021qCeRi3yUBtqd2UFj1c0RQ.jpg",
+			original_language: "en",
+			original_title: "12 Strong",
+			genre_ids: [
+				10752,
+				18,
+				36
+
+			],
+			backdrop_path: "/wacEk5YSNE41QeKseilyytksXmv.jpg",
+			adult: false,
+			overview: "A team of CIA agents and special forces head into Afghanistan in the aftermath of the September 11th attacks in an attempt to dismantle the Taliban.",
+			release_date: "2018-01-18"
+
+		},
+		{
+			vote_count: 2,
+			id: 454619,
+			video: false,
+			vote_average: 0,
+			title: "Overboard",
+			popularity: 37.449871,
+			poster_path: "/gaHknuun1VPPugkTsSfB8PejKVg.jpg",
+			original_language: "en",
+			original_title: "Overboard",
+			genre_ids: [
+				10749,
+				35
+
+			],
+			backdrop_path: "/yCfXPtYz1CtPw4bbjAZ8xwXenNS.jpg",
+			adult: false,
+			overview: "A spoiled, wealthy yacht owner is thrown overboard and becomes the target of revenge from his mistreated employee. A remake of the 1987 comedy.",
+			release_date: "2018-05-03"
+
+		},
+		{
+			vote_count: 45,
+			id: 460019,
+			video: false,
+			vote_average: 5.9,
+			title: "Truth or Dare",
+			popularity: 36.274483,
+			poster_path: "/zbvziwnZa91AJD78Si0hUb5JP5X.jpg",
+			original_language: "en",
+			original_title: "Truth or Dare",
+			genre_ids: [
+				53,
+				27
+
+			],
+			backdrop_path: "/eQ5xu2pQ5Kergubto5PbbUzey28.jpg",
+			adult: false,
+			overview: "A harmless game of “Truth or Dare” among friends turns deadly when someone—or something—begins to punish those who tell a lie—or refuse the dare.",
+			release_date: "2018-04-12"
+
+		},
+		{
+			vote_count: 1,
+			id: 383498,
+			video: false,
+			vote_average: 0,
+			title: "Deadpool 2",
+			popularity: 33.90471,
+			poster_path: "/zWoFYfjEArXKjTJX6akSveGYSs3.jpg",
+			original_language: "en",
+			original_title: "Deadpool 2",
+			genre_ids: [
+				28,
+				35,
+				878
+
+			],
+			backdrop_path: "/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg",
+			adult: false,
+			overview: "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.",
+			release_date: "2018-05-15"
+
+		},
+		{
+			vote_count: 74,
+			id: 437557,
+			video: false,
+			vote_average: 6.3,
+			title: "Blockers",
+			popularity: 28.393267,
+			poster_path: "/cx0LpfM6Drla8uzFfMT09uqKPRu.jpg",
+			original_language: "en",
+			original_title: "Blockers",
+			genre_ids: [
+				35
+
+			],
+			backdrop_path: "/bStCGzVYFrKxtOyThKkqgI8gjgE.jpg",
+			adult: false,
+			overview: "When three parents discover their daughters’ pact to lose their virginity at prom, they launch a covert one-night operation to stop the teens from sealing the deal.",
+			release_date: "2018-03-30"
+
+		},
+		{
+			vote_count: 4,
+			id: 348350,
+			video: false,
+			vote_average: 0,
+			title: "Solo: A Star Wars Story",
+			popularity: 27.917735,
+			poster_path: "/3IGbjc5ZC5yxim5W0sFING2kdcz.jpg",
+			original_language: "en",
+			original_title: "Solo: A Star Wars Story",
+			genre_ids: [
+				28,
+				12,
+				10751,
+				878
+
+			],
+			backdrop_path: "/96B1qMN9RxrAFu6uikwFhQ6N6J9.jpg",
+			adult: false,
+			overview: "Through a series of daring escapades deep within a dark and dangerous criminal underworld, Han Solo meets his mighty future copilot Chewbacca and encounters the notorious gambler Lando Calrissian.",
+			release_date: "2018-05-23"
+
+		},
+		{
+			vote_count: 198,
+			id: 395990,
+			video: false,
+			vote_average: 4.8,
+			title: "Death Wish",
+			popularity: 22.702841,
+			poster_path: "/zRpOwIlOXZZVWgDernbPbdHU5cl.jpg",
+			original_language: "en",
+			original_title: "Death Wish",
+			genre_ids: [
+				28,
+				80,
+				18,
+				53
+
+			],
+			backdrop_path: "/uQqQvmptJLPTcWDrZXn22p7j7s3.jpg",
+			adult: false,
+			overview: "A mild-mannered father is transformed into a killing machine after his family is torn apart by a violent act.",
+			release_date: "2018-03-02"
+
+		},
+		{
+			vote_count: 231,
+			id: 398181,
+			video: false,
+			vote_average: 6.4,
+			title: "You Were Never Really Here",
+			popularity: 21.405159,
+			poster_path: "/ktPbjD6nYg5NxmeTt72Iqaw5GD0.jpg",
+			original_language: "en",
+			original_title: "You Were Never Really Here",
+			genre_ids: [
+				18,
+				53,
+				80
+
+			],
+			backdrop_path: "/g2yZ1kiGwLO5AUSmtsEJeFEWghJ.jpg",
+			adult: false,
+			overview: "A traumatised veteran, unafraid of violence, tracks down missing girls for a living. When a job spins out of control, Joe's nightmares overtake him as a conspiracy is uncovered leading to what may be his death trip or his awakening.",
+			release_date: "2017-11-08"
+
+		},
+		{
+			vote_count: 543,
+			id: 446791,
+			video: false,
+			vote_average: 6.2,
+			title: "All the Money in the World",
+			popularity: 21.202256,
+			poster_path: "/q6nE9Hf0ezszjI4DbCxwzQ73MMy.jpg",
+			original_language: "en",
+			original_title: "All the Money in the World",
+			genre_ids: [
+				80,
+				18,
+				36,
+				53
+
+			],
+			backdrop_path: "/j3gpAEqIXyFj54eA1d7WI93LFuf.jpg",
+			adult: false,
+			overview: "The story of the kidnapping of 16-year-old John Paul Getty III and the desperate attempt by his devoted mother to convince his billionaire grandfather Jean Paul Getty to pay the ransom.",
+			release_date: "2017-12-13"
+
+		},
+		{
+			vote_count: 237,
+			id: 399174,
+			video: false,
+			vote_average: 7.9,
+			title: "Isle of Dogs",
+			popularity: 20.149871,
+			poster_path: "/c0nUX6Q1ZB0P2t1Jo6EeFSVnOGQ.jpg",
+			original_language: "en",
+			original_title: "Isle of Dogs",
+			genre_ids: [
+				12,
+				35,
+				16
+
+			],
+			backdrop_path: "/5YtXsLG9nc"
+		}
+	];
 }
 
 export function getImg(movie) {
 	return "http://image.tmdb.org/t/p/w185/" + movie.poster_path; 
+}
+
+export function getLine() {
+	return [
+	{
+		    "symbol": "MSFT",
+		    "date": "Jan 2000",
+		    "price": 39.81
+		  
+	},
+		{
+			    "symbol": "MSFT",
+			    "date": "Feb 2000",
+			    "price": 36.35
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Mar 2000",
+			    "price": 43.22
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Apr 2000",
+			    "price": 28.37
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "May 2000",
+			    "price": 25.45
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Jun 2000",
+			    "price": 32.54
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Jul 2000",
+			    "price": 28.4
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Aug 2000",
+			    "price": 28.4
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Sep 2000",
+			    "price": 24.53
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Oct 2000",
+			    "price": 28.02
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Nov 2000",
+			    "price": 23.34
+			  
+		},
+		{
+			    "symbol": "MSFT",
+			    "date": "Dec 2000",
+			    "price": 17.65
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Jan 2000",
+			    "price": 64.56
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Feb 2000",
+			    "price": 68.87
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Mar 2000",
+			    "price": 67
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Apr 2000",
+			    "price": 55.19
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "May 2000",
+			    "price": 48.31
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Jun 2000",
+			    "price": 36.31
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Jul 2000",
+			    "price": 30.12
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Aug 2000",
+			    "price": 41.5
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Sep 2000",
+			    "price": 38.44
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Oct 2000",
+			    "price": 36.62
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Nov 2000",
+			    "price": 24.69
+			  
+		},
+		{
+			    "symbol": "AMZN",
+			    "date": "Dec 2000",
+			    "price": 15.56
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Jan 2000",
+			    "price": 100.52
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Feb 2000",
+			    "price": 92.11
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Mar 2000",
+			    "price": 106.11
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Apr 2000",
+			    "price": 99.95
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "May 2000",
+			    "price": 96.31
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Jun 2000",
+			    "price": 98.33
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Jul 2000",
+			    "price": 100.74
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Aug 2000",
+			    "price": 118.62
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Sep 2000",
+			    "price": 101.19
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Oct 2000",
+			    "price": 88.5
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Nov 2000",
+			    "price": 84.12
+			  
+		},
+		{
+			    "symbol": "IBM",
+			    "date": "Dec 2000",
+			    "price": 76.47
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Jan 2000",
+			    "price": 25.94
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Feb 2000",
+			    "price": 28.66
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Mar 2000",
+			    "price": 33.95
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Apr 2000",
+			    "price": 31.01
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "May 2000",
+			    "price": 21
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Jun 2000",
+			    "price": 26.19
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Jul 2000",
+			    "price": 25.41
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Aug 2000",
+			    "price": 30.47
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Sep 2000",
+			    "price": 12.88
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Oct 2000",
+			    "price": 9.78
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Nov 2000",
+			    "price": 8.25
+			  
+		},
+		{
+			    "symbol": "AAPL",
+			    "date": "Dec 2000",
+			    "price": 7.44
+			  
+		}
+
+	];
+}
+
+export function getBar() {
+	return [
+	{
+		    "Country_Name": "Afghanistan",
+		    "Country_ISO3": "AFG",
+		    "Country_ISO2": "AF",
+		    "Year": 2010,
+		    "GDP_MUSD": 15936.78,
+		    "GDP_pCapita_KUSD": 0.56,
+		    "Aid_MUSD": 0,
+		    "Milit_Exp_MUSD": 576,
+		    "Milit_pCapita_USD": 20.28
+		  
+	},
+		{
+			    "Country_Name": "Albania",
+			    "Country_ISO3": "ALB",
+			    "Country_ISO2": "AL",
+			    "Year": 2010,
+			    "GDP_MUSD": 11926.96,
+			    "GDP_pCapita_KUSD": 4.18,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 190,
+			    "Milit_pCapita_USD": 60.31
+			  
+		},
+		{
+			    "Country_Name": "Algeria",
+			    "Country_ISO3": "DZA",
+			    "Country_ISO2": "DZ",
+			    "Year": 2010,
+			    "GDP_MUSD": 161207.3,
+			    "GDP_pCapita_KUSD": 4.35,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 5671,
+			    "Milit_pCapita_USD": 153.01
+			  
+		},
+		{
+			    "Country_Name": "Angola",
+			    "Country_ISO3": "AGO",
+			    "Country_ISO2": "AO",
+			    "Year": 2010,
+			    "GDP_MUSD": 82470.89,
+			    "GDP_pCapita_KUSD": 4.22,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 3501,
+			    "Milit_pCapita_USD": 179.09
+			  
+		},
+		{
+			    "Country_Name": "Antigua and Barbuda",
+			    "Country_ISO3": "ATG",
+			    "Country_ISO2": "AG",
+			    "Year": 2010,
+			    "GDP_MUSD": 1135.54,
+			    "GDP_pCapita_KUSD": 13.02,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": "NA",
+			    "Milit_pCapita_USD": "NA"
+			  
+		},
+		{
+			    "Country_Name": "Argentina",
+			    "Country_ISO3": "ARG",
+			    "Country_ISO2": "AR",
+			    "Year": 2010,
+			    "GDP_MUSD": 462703.79,
+			    "GDP_pCapita_KUSD": 11.46,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 3476,
+			    "Milit_pCapita_USD": 86.09
+			  
+		},
+		{
+			    "Country_Name": "Armenia",
+			    "Country_ISO3": "ARM",
+			    "Country_ISO2": "AM",
+			    "Year": 2010,
+			    "GDP_MUSD": 9260.29,
+			    "GDP_pCapita_KUSD": 3.12,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 395,
+			    "Milit_pCapita_USD": 133.29
+			  
+		},
+		{
+			    "Country_Name": "Aruba",
+			    "Country_ISO3": "ABW",
+			    "Country_ISO2": "AW",
+			    "Year": 2010,
+			    "GDP_MUSD": 2467.7,
+			    "GDP_pCapita_KUSD": 24.29,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": "NA",
+			    "Milit_pCapita_USD": "NA"
+			  
+		},
+		{
+			    "Country_Name": "Australia",
+			    "Country_ISO3": "AUS",
+			    "Country_ISO2": "AU",
+			    "Year": 2010,
+			    "GDP_MUSD": 1141267.76,
+			    "GDP_pCapita_KUSD": 51.8,
+			    "Aid_MUSD": 4355.15,
+			    "Milit_Exp_MUSD": 23221,
+			    "Milit_pCapita_USD": 1052.38
+			  
+		},
+		{
+			    "Country_Name": "Austria",
+			    "Country_ISO3": "AUT",
+			    "Country_ISO2": "AT",
+			    "Year": 2010,
+			    "GDP_MUSD": 389656.07,
+			    "GDP_pCapita_KUSD": 46.44,
+			    "Aid_MUSD": 713.84,
+			    "Milit_Exp_MUSD": 3219,
+			    "Milit_pCapita_USD": 383.68
+			  
+		},
+		{
+			    "Country_Name": "Azerbaijan",
+			    "Country_ISO3": "AZE",
+			    "Country_ISO2": "AZ",
+			    "Year": 2010,
+			    "GDP_MUSD": 52902.7,
+			    "GDP_pCapita_KUSD": 5.84,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 1476,
+			    "Milit_pCapita_USD": 163.02
+			  
+		},
+		{
+			    "Country_Name": "Bahamas The",
+			    "Country_ISO3": "BHS",
+			    "Country_ISO2": "BS",
+			    "Year": 2010,
+			    "GDP_MUSD": 7910,
+			    "GDP_pCapita_KUSD": 21.94,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": "NA",
+			    "Milit_pCapita_USD": "NA"
+			  
+		},
+		{
+			    "Country_Name": "Bahrain",
+			    "Country_ISO3": "BHR",
+			    "Country_ISO2": "BH",
+			    "Year": 2010,
+			    "GDP_MUSD": 25713.54,
+			    "GDP_pCapita_KUSD": 20.55,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 776,
+			    "Milit_pCapita_USD": 620.05
+			  
+		},
+		{
+			    "Country_Name": "Bangladesh",
+			    "Country_ISO3": "BGD",
+			    "Country_ISO2": "BD",
+			    "Year": 2010,
+			    "GDP_MUSD": 115279.08,
+			    "GDP_pCapita_KUSD": 0.76,
+			    "Aid_MUSD": 0,
+			    "Milit_Exp_MUSD": 1413,
+			    "Milit_pCapita_USD": 9.35
+			  
+		}
+
+	];
 }
