6ff9bc0299a7a750a417f6b8989cae8b167cea0b
William Ravelo <drummerwilliam@gmail.com>
2018-05-16 18:51:48 -0500

    added bar char

--- imports/charts/BarChart.js
+++ imports/charts/BarChart.js
@@ -1,50 +1,147 @@
-import { getNowP, getUpc, getImg, getLine, getBar } from "../data.js";
 import * as d3 from "d3";
 
-export function barChart() {
-	var svg = d3.select("body")
-		.append("svg")
-		.attr("width", 905)
-		.attr("height", 600);
+export function barChart(){
+	var width = 1000,
+		barHeight = 25,
+		numBars = 10,
+		barMargin = 35,
+		thumbWidth = 25,
+		title = "Chart",
+		type = "popularity",
+		chartMargin = {
+			top: 20,
+			left: 20,
+			bottom: 70,
+			right: 20	      
+		};
 
-	var data = getNowP();
-	data = data.slice(0,36);
+	function chart(selection){
+		selection.each(function(data){
+			numBars = data.length;
 
-	var rects = svg.selectAll("rect")
-		.data(data)
-		.enter()
-		.append("rect");
+			var ratings = data.map(function(d){
+				return Number(d[type]);
+			});
 
-	rects.attr("x", 105)
-		.attr("y", function(d, i) {
-			return i * 15;
+			var height = (barHeight + barMargin) * numBars,
+				outerHeight = height + chartMargin.top + chartMargin.bottom,
+				outerWidth = width + chartMargin.left + chartMargin.right;
 
-		})
-		.attr("width", function(d) {
-			return + d.vote_count * 800/35000 ;
 
-		})
-		.attr("height", 13)
-		.append("title")
-		.text(function(d) {
-			return d.title;
+			var svg = d3.select(this)
+				.attr('width',width)
+				.attr('viewBox', '0 0 ' + outerWidth + ' ' + outerHeight)
+				.append('g')
+				.attr('transform', 'translate(' + chartMargin.left + ',' + chartMargin.top + ')');
+
+			var x = d3.scaleLinear()
+				.domain([0, d3.max(ratings)])
+				.range([0, width]);
+
+			var bars = svg.append('svg')
+				.attr('height', height)
+				.attr('class', 'bars');
+
+			var bar = bars.selectAll('.movie')
+				.data(data)
+				.enter().append('g')
+				.attr('class', 'movie')
+				.attr('fill', 'url(#coolGradient)')
+				.attr('transform', function(d, i){
+					return 'translate(0, ' + i * (barHeight + barMargin) + ')';
+				});
+
+			bar.append('rect')
+				.attr('width', function(d){
+					return x(d[type]);
+				})
+				.attr('height', barHeight)
+				.attr('transform', 'translate(0,' + barMargin/4 + ')');
+
+			bar.append('image')
+				.attr('width', thumbWidth)
+				.attr('height', thumbWidth)
+				.attr('y', barMargin/4)
+				.attr('xlink:href', function(d){
+					const image = "http://image.tmdb.org/t/p/w500/" + d.poster_path + ".jpg";
+					console.log(image);
+					return image; 
+				});
+
+			var barText = bar.append('text')
+				.attr('x', function(d){
+					return x(d[type]);
+				})
+				.attr('y', barMargin/4 + barHeight)
+				.attr('dy', '1em')
+				.attr('x', 3);
+
+			barText.append('tspan')
+				.attr('class', 'bar_title')
+				.attr('dx', 10)
+				.text(function(d){
+					return d.title;
+				});
+
+			var xAxis = d3.axisBottom()
+				.scale(x)
+				.tickSize(5, 1)
+				.tickPadding(5);
+
+
+			svg.append('g')
+				.attr('class', 'x axis')
+				.call(xAxis)
+				.attr('transform', 'translate(0,' + height + ')');
+
+			svg.selectAll('.y.axis .tick text')
+				.attr('dy', '0');
+
+			svg.append('text')
+				.attr('class','axisLabel')
+				.attr('y', height)
+				.attr('x', width/2)
+				.attr('dy', '3em')
+				.attr('text-anchor', 'middle')
+				.text(title);	    
 		});
+	}
 
-	var countries = svg.selectAll("text")
-		.data(data)
-		.enter()
-		.append("text");
+	chart.orderByRank = function() {
+		d3.selectAll('.movie')
+			.transition(700)
+			.attr('fill', 'url(#warmGradient)')
+			.attr('transform', function(d){
+				return 'translate(0,' + (d.rank - 1) * (barHeight + barMargin) + ')';	    
+			});
+		return chart;
+	};
 
-	countries.attr("x", 100)
-		.attr("y", function(d,i) {
-			return i * 15 + 11; 
-		})
-		.attr("fill", "black")
-		.attr("font-size", "10px")
-		.attr("font-weight", "normal")
-		.attr("font-family", "sans-serif")
-		.attr("text-anchor", "end")
-		.text(function(d) {
-			return d.title
-		});
+	chart.orderByPlayCount = function() {
+		d3.selectAll('.movie')
+			.transition(700)
+			.attr('fill', 'url(#coolGradient)')
+			.attr('transform', function(d, i){
+				return 'translate(0,' + i * (barHeight + barMargin) + ')';	    
+			});
+		return chart;	  
+	};
+
+	chart.chartTitle = function(value) {
+		if (!arguments.length) {
+			return title;
+		}
+		title = value;
+		return chart;
+	};
+
+	chart.chartType = function(value) {
+		if(!arguments.length) {
+			return type;
+		}
+		type = value;
+		return chart;
+	}
+
+	return chart;
 }
--- imports/ui/pages/PopularPage.jsx
+++ imports/ui/pages/PopularPage.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Button } from "reactstrap";
-import { Meteor } from "meteor/meteor";
-import { withRouter } from "react-router-dom";
+import { Meteor } from "meteor/meteor";import { withRouter } from "react-router-dom";
 import { withTracker } from "meteor/react-meteor-data";
 import TweetList from "../components/TweetList.jsx";
 import MoviesList from "../components/MoviesList.jsx";
-
+import { barChart } from "../../charts/BarChart.js";
+import * as d3 from "d3";
+import "../css/BarChart.css";
 
 class PopularPage extends Component {
 	constructor(props) {
@@ -16,17 +17,50 @@ class PopularPage extends Component {
 	}
 
 	componentDidMount() {
+		var myChart = barChart()
+		myChart.chartTitle("Popularity")
+				.chartType("popularity");
+
+		var rankedAlbums = [
+			{
+				popularity: 25.526805,
+				vote_average: 8.3,
+				poster_path:  "adw6Lq9FiC9zjYEpOqfq03ituwp",
+				title: "Fight Club"
+			},
+			{
+				popularity: 25.526805,
+				vote_average: 8.3,
+				poster_path:  "adw6Lq9FiC9zjYEpOqfq03ituwp",
+				title: "Fight Club"
+			},
+			{
+				popularity: 25.526805,
+				vote_average: 8.3,
+				poster_path:  "adw6Lq9FiC9zjYEpOqfq03ituwp",
+				title: "Fight Club"
+			}
+		];
+		d3.select('#chart')
+			.datum(rankedAlbums)
+			.call(myChart);
 	}
 
 	render() {
 		return (
 			<Container>
+				<Row className="graph_container bar_chart">
+					<svg id="chart" className="movieChart">
+						<defs>
+							<linearGradient id="coolGradient" x1="0" x2="100%" y1="0" y2="0" gradientUnits="userSpaceOnUse">
+								<stop stopColor="#3DAFD5" offset="0%"/>
+								<stop stopColor="#8AF2B3" offset="100%"/>
+							</linearGradient>
+						</defs>
+					</svg>
+				</Row>
 				<Row>
-					<Col md="8">
-						<MoviesList type="popular" />
-					</Col>
-					<Col md="4">
-					</Col>
+					<MoviesList type="popular" />
 				</Row>
 
 			</Container>
