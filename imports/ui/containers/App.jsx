d105e149eb41b9c0896c3a876933d9515c8964d8
William Ravelo <drummerwilliam@gmail.com>
2018-05-15 22:07:26 -0500

    now the navbar changes depending on auth

--- imports/ui/containers/App.jsx
+++ imports/ui/containers/App.jsx
@@ -5,11 +5,11 @@ import { withRouter  } from "react-router-dom";
 import { withTracker  } from "meteor/react-meteor-data";
 import { Link } from "react-router-dom";
 
-export default class App extends Component {
+class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			
 		};
 	}
 
@@ -25,9 +25,12 @@ export default class App extends Component {
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"  className="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
 						</Link>
 						<Link to="/popular" className="py-2 d-none d-md-inline-block" href="#">Popular</Link>
-						<Link to="theaters" className="py-2 d-none d-md-inline-block" href="#">In Theaters</Link>
-						<Link to="soon" className="py-2 d-none d-md-inline-block" href="#">Coming Soon</Link>
-						<Link to="sign" className="py-2 d-none d-md-inline-block" href="#">Log In</Link>
+						<Link to="/theaters" className="py-2 d-none d-md-inline-block" href="#">In Theaters</Link>
+						<Link to="/soon" className="py-2 d-none d-md-inline-block" href="#">Coming Soon</Link>
+						{ !this.props.userId ?  
+								<Link to="/sign" className="py-2 d-none d-md-inline-block" href="#">Log Out</Link> :
+								<Link to="/" className="py-2 d-none d-md-inline-block">Buenas</Link>
+						}
 					</Container>
 				</nav>
 				{this.props.children}
@@ -35,3 +38,11 @@ export default class App extends Component {
 		);
 	}
 }
+
+export default withRouter(
+	withTracker((props) => {
+		return {
+			userId: Meteor.userId()
+		};
+	})(App)
+);
