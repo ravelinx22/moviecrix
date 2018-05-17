d569f053254d9759eafa6ec79d990f23ff446ea0
William Ravelo <drummerwilliam@gmail.com>
2018-05-16 23:37:19 -0500

    finished timeline

--- imports/ui/pages/SoonPage.jsx
+++ imports/ui/pages/SoonPage.jsx
@@ -16,60 +16,18 @@ class SoonPage extends Component {
 		}
 	}
 
-	componentDidMount() {
-		var serverData = [
-			{
-				vote_count: 3504,
-				id: 299536,
-				video: false,
-				vote_average: 8.5,
-				title: "Avengers: Infinity War",
-				popularity: 703.53968,
-				poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-				original_language: "en",
-				original_title: "Avengers: Infinity War",
-				genre_ids: [
-					12,
-					878,
-					14,
-					28
-
-				],
-				backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-				adult: false,
-				overview: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
-				release_date: "2018-07-25"
-			},
-			{
-				vote_count: 552,
-				id: 427641,
-				video: false,
-				vote_average: 5.9,
-				title: "Rampage",
-				popularity: 169.383611,
-				poster_path: "/30oXQKwibh0uANGMs0Sytw3uN22.jpg",
-				original_language: "en",
-				original_title: "Rampage",
-				genre_ids: [
-					28,
-					12,
-					878
-
-				],
-				backdrop_path: "/wrqUiMXttHE4UBFMhLHlN601MZh.jpg",
-				adult: false,
-				overview: "Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth. But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size. To make matters worse, it’s soon discovered there are other similarly altered animals. As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.",
-				release_date: "2018-06-12"
-			}
-		]
-		var dat = this.formatData(serverData);
-		this.renderTimeLine(dat);
+	componentDidMount() {	
+		Meteor.call("movies.getUpcoming", 1, (error, res) => {
+			var dat = this.formatData(res.slice(0,10));
+			this.renderTimeLine(dat);
+		});
 	}
 
 	renderTimeLine(data) {
+		const that = this;
 		var chart = timelines()
-			.beginning(new Date()) 
-			.ending(new Date(new Date().setFullYear(new Date().getFullYear() + 1)))
+			.beginning(this.getMinDate(data)) 
+			.ending(this.getMaxDate(data))
 			.showTimeAxisTick() 
 			.stack() 
 			.margin({left:70, right:30, top:0, bottom:0})
@@ -80,22 +38,27 @@ class SoonPage extends Component {
 				tickSize: 6,
 				tickValues: null
 			}).hover(function (d, i, datum) {
-				const title = d.title;
+				const id = d.movie_id;
+				that.setState({
+					selected_movie: d.title + " - " + d.starting_time
+				})
 			})
 			.click(function (d, i, datum) {
-				const title = d.title;
-				alert(title);
+				const id = d.movie_id;
+				that.props.history.push("/detail/" + id);
 			})
 
 		var svg = d3.select("#timeline1")
 			.append("svg")
 			.attr("width", "100%")
+			.attr("height", (30*data.length)+"px")
 			.datum(data)
 			.call(chart);
 	}
 
 	formatData(data) {
 		return data.map((d) => {
+			console.log(d);
 			return {
 				class: d.title,
 				icon: "http://image.tmdb.org/t/p/w500" + d.poster_path,
@@ -103,6 +66,7 @@ class SoonPage extends Component {
 					{
 						starting_time: new Date(d.release_date),
 						display: "circle",
+						movie_id: d.id,
 						title: d.title
 					}
 				]
@@ -110,18 +74,43 @@ class SoonPage extends Component {
 		});
 	}
 
+	getMinDate(data) {
+		var min = new Date();
+		for(var i = 0; i < data.length; i++) {
+			if(min > data[i].times[0].starting_time) {
+				min = data[i].times[0].starting_time;
+			}
+		}
+		return min;
+	}
+
+	getMaxDate(data) {
+		var max = new Date();
+		for(var i = 0; i < data.length; i++) {
+			if(max < data[i].times[0].starting_time) {
+				max = data[i].times[0].starting_time;
+			}
+		}
+		return max;
+	}
+
 
 	render() {
+		const style = {
+			textAlign: "center"
+		}
+
 		return (
 			<Container>
-				<h1>In theaters</h1>
-				<div id="timeline1"></div>
+				<h1>Coming Soon</h1>
+				<Row className="timeline_row">
+					<Col md="12">
+						<div id="timeline1"></div>
+						<h3 ref="movie_title" style={style}>{this.state.selected_movie ? this.state.selected_movie : "Select a movie from the chart"}</h3>
+					</Col>
+				</Row>
 				<Row>
-					<Col md="8">
-						<MoviesList type="coming-soon" />
-					</Col>
-					<Col md="4">
-					</Col>
+					<MoviesList type="coming-soon" />
 				</Row>
 			</Container>
 		);
