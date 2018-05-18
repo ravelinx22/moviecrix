a7fddc75aa935e97409bce898c144533bde0f68f
naguilar12 <n.aguilar@uniandes.edu.co>
2018-05-18 03:00:49 -0500

    Audits and security

--- server/main.js
+++ server/main.js
@@ -2,3 +2,47 @@ import "../imports/api/Movies";
 import "../imports/api/Reviews";
 import "../imports/api/Tweets";
 import "../imports/api/FavoriteMovies";
+import { WebApp } from 'meteor/webapp';
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter';
+
+
+WebApp.addHtmlAttributeHook(() => ({ lang: 'en' }));
+
+// Deny all client-side updates to user documents
+Meteor.users.deny({
+    update() { return true; }
+});
+
+const methods = [
+    "favoriteMovies.addFavorite",
+    "favoriteMovies.deleteFromFavorites",
+    "favoriteMovies.getFavorites",
+    "movies.getPopular",
+    "movies.getTopRated",
+    "movies.getUpcoming",
+    "movies.nowPlaying",
+    "movies.randomNowPlaying",
+    "movies.getGenres",
+    "movies.getByGenre",
+    "movies.getByName",
+    "movies.getSpecificMovie",
+    "movies.recommendedByMovie",
+    "reviews.reviewMovie"
+]
+
+// Only allow 10 list operations per connection per second
+if (Meteor.isServer) {
+    var requestLimit = 10;
+    var requestTimeout = 1000;
+
+    methods.map((met)=>{
+        DDPRateLimiter.addRule({
+            type: "method",
+            name: met,
+        }, requestLimit, requestTimeout);
+    })
+    
+}
+
+
+
